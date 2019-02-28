019af5ecf61c63030f2f4ad17263c272de5cf86f
Shym Barrett <shymjamin@yahoo.com>
2019-02-28 00:53:18 -0600

    The basics

---
+++ final.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+
+    $(".top-right, .top-left, .middle-left, .middle-right, .bottom-left, .bottom-right").hover(function(){
+        $(this).css("background-color", "#00ffd05b");
+        }, function(){
+        $(this).css("background-color", "transparent");
+      });
+
+    $(".top-right, .top-left, .middle-left, .middle-right, .bottom-left, .bottom-right").click(function() {
+        var elbow = $(this).attr("title");
+        $(".highlight").html(elbow);
+    });
+
+});
+
+function updateTextInput(val) {
+    document.getElementById('textInput').value=val; 
+    };
